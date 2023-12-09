fc29947e9cc2f15dd120de4925619b9741da72f8
VIRGINIE  ROSA_ARSENEV <virginieralg@gmail.com>
2023-12-09 11:00:58 +0100

    quete14 étape 1 finie

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -7,6 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CategoryRepository;
 Use App\Repository\ProgramRepository;
+use App\Form\CategoryType;
+use App\Entity\Category;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 #[Route('/category', name: 'category_')]
 class CategoryController extends AbstractController
@@ -23,6 +27,32 @@ class CategoryController extends AbstractController
 
     }
 
+    #[Route('/new', name: 'new')]
+    public function new(Request $request, EntityManagerInterface $entityManager) : Response
+        {
+        // Create a new Category Object
+        $category = new Category();
+        // Create the associated Form
+        $form = $this->createForm(CategoryType::class, $category);
+        // Get data from HTTP request
+        $form->handleRequest($request);
+        // Was the form submitted ?
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // For example : persiste & flush the entity
+
+            if ($form->isSubmitted() && $form->isValid()) {
+              $entityManager->persist($category);
+              $entityManager->flush();
+              return $this->redirectToRoute('category_index');
+            }
+          
+            // And redirect to a route that display the result
+        }
+            return $this->render('category/new.html.twig', [
+                'form' => $form,
+            ]);
+        }
 
     #[Route('/show/{categoryName}', name: 'show')]
     public function show(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository):Response
@@ -43,4 +73,7 @@ class CategoryController extends AbstractController
             'programs' => $programs,
         ]);
     }
+
+    
+   
         }
